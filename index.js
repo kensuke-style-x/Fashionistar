adc6c6b7cbb6a2a2cafd86deae684fe9e2ae33e6
kensuke-style-x <66291400+kensuke-style-x@users.noreply.github.com>
2024-12-05 11:48:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+let starImage = document.querySelector('Star');
+
+starImage.onclick = function() {
+
+  let starSrc = starImage.getAtrribute('src');
+
+  if(starSrc === 'Star/IMG_E5683.JPG'){
+
+    starImage.setAttribute('src','Star/thisWeeksStar');
+
+  } else {
+
+    starImage.setAttribue('src','Star/IMG_E5683.JPG');
+  }
+}// function
+
 
