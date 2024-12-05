933cdbf88133ba477ff31a43d916fc78b6169f54
kensuke-style-x <66291400+kensuke-style-x@users.noreply.github.com>
2024-12-05 11:55:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-let starImage = document.querySelector(".Star");
+let starImage = document.querySelector('.Star');
 
 starImage.onclick = function() {
 
   let starSrc = starImage.getAtrribute('src');
 
-  if(starSrc === "Star/IMG_E5683.JPG"){
+  if(starSrc === 'Star/IMG_E5683.JPG'){
 
-    starImage.setAttribute('src',"Star/thisWeeksStar");
+    starImage.setAttribute('src','Star/thisWeeksStar');
 
   } else {
 
-    starImage.setAttribue('src',"Star/IMG_E5683.JPG");
+    starImage.setAttribue('src','Star/IMG_E5683.JPG');
   }
 }// function
 
