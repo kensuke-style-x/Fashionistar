cc96d464e985118dd9b71006e8587321c255d311
kensuke-style-x <66291400+kensuke-style-x@users.noreply.github.com>
2024-12-14 10:45:40 +0900

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ starImage.onclick = function() {
 
   if(starSrc === 'Star/IMG_E5683.JPG'){
 
-    starImage.setAttribute('src','Star/Seven Star App Event Image .001.jpeg');
+    starImage.setAttribute('src','Star/IMG_C6CACB121321-1.jpeg');
 
   } else {
 
