59347dac2ebfb89825d40c0c0e75c7a855d837b8
kensuke-style-x <66291400+kensuke-style-x@users.noreply.github.com>
2024-12-14 10:28:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ starImage.onclick = function() {
 
   if(starSrc === 'Star/IMG_E5683.JPG'){
 
-    starImage.setAttribute('src','Star/thisWeeksStar');
+    starImage.setAttribute('src','Star/IMG_C99D7C58EF2B-1.jpeg');
 
   } else {
 
