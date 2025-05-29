b1d9544f34e39471b0ab8e4590b5ede29719e8bb
kensuke-style-x <66291400+kensuke-style-x@users.noreply.github.com>
2025-05-29 11:05:01 +0900

    Update index.js

--- privacy/index.js
+++ privacy/index.js
@@ -1,25 +1,19 @@
 //translate
 function showLanguage(lang){
   
- const languages = ['en','jp','ch_simplify','ch_traditional','fr','it','es','es_latino'];
-  languages.forEach(id => {
-     const section = document.getElementById(id);
-    if (section) {
-      section.classList.remove('active');
-      section.style.display = 'none';
-    }
+ const sections = document.querySelectorAll('.lang-section');
+  sections.forEach(section => {
+    section.style.display = 'none';
   });
+    
 
   const targetSection = document.getElementById(lang)
   if (targetSection){
-    targetSection.classList.add('active');
     targetSection.style.display = 'block';
   }
 }
 
 //button
-document.addEventListener('DOMContentLoaded',() => {
-
   document.getElementById('en-btn').addEventListener('click', () => showLanguage('en'));
   document.getElementById('jp-btn').addEventListener('click', () => showLanguage('jp'));
   document.getElementById('ch_simplify-btn').addEventListener('click', () => showLanguage('ch_simplify'));
