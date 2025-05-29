4cb4f8cc01d84c03ed45132ea63f3ea891842860
kensuke-style-x <66291400+kensuke-style-x@users.noreply.github.com>
2025-05-29 11:46:07 +0900

    Update index.js

--- subscription/index.js
+++ subscription/index.js
@@ -2,26 +2,18 @@
 
 function showLanguage(lang){
   
- const languages = ['en','jp','ch_simplify','ch_traditional','fr','it','es','es_latino'];
-  languages.forEach(id => {
-     const section = document.getElementById(id);
-    if (section) {
-      section.classList.remove('active');
+ const sections = document.querySelectorAll('.lang-section');
+  sections.forEach(section => {
       section.style.display = 'none';
-    }
   });
 
   const targetSection = document.getElementById(lang)
   if (targetSection){
-    targetSection.classList.add('active');
     targetSection.style.display = 'block';
   }
 }
 
 //button
-document.addEventListener('DOMContentLoaded',() => {
-
-
   document.getElementById('en-btn').addEventListener('click', () => showLanguage('en'));
   document.getElementById('jp-btn').addEventListener('click', () => showLanguage('jp'));
   document.getElementById('ch_simplify-btn').addEventListener('click', () => showLanguage('ch_simplify'));
@@ -30,5 +22,7 @@ document.addEventListener('DOMContentLoaded',() => {
   document.getElementById('it-btn').addEventListener('click', () => showLanguage('it'));
   document.getElementById('es-btn').addEventListener('click', () => showLanguage('es'));
   document.getElementById('es_latino-btn').addEventListener('click', () => showLanguage('es_latino'));
-  
+
+  window.addEventListener('DOMContentLoaded', () => {
+    showLanguage('en');
 });
